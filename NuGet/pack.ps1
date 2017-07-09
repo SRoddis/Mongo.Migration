215fc53a596aca0246cc15e9d4e24f00f90ed772
sroddis <sean_roddis@hotmail.de>
2017-07-09 19:01:55 +0200

    added nuget package

---
+++ NuGet/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\Mongo.Migration\bin\Release\Mongo.Migration.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\Mongo.Migration.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\Mongo.Migration.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\nuget\Mongo.Migration.compiled.nuspec
