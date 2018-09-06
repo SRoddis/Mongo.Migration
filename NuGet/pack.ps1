dbcae42a2ca1b40181afdb858a67908de29c49d3
Sean Roddis <sean_roddis@hotmail.de>
2018-09-06 15:35:51 +0200

    .net core version patch

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,8 +1,6 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-#$version = [System.Reflection.Assembly]::LoadFile("$root\Mongo.Migration\bin\Release\net45\Mongo.Migration.dll").GetName().Version
-#$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
-
-$versionStr = "1.1.0"
+$version = [System.Reflection.Assembly]::LoadFile("$root\Mongo.Migration\bin\Release\net45\Mongo.Migration.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
