942da27dd8120bc2434b6be86806c75b9603deef
Sean Roddis <sean_roddis@hotmail.de>
2018-09-06 14:58:27 +0200

    changed nuget pack path for version

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,5 +1,5 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-$version = [System.Reflection.Assembly]::LoadFile("$root\Mongo.Migration\bin\Release\Mongo.Migration.dll").GetName().Version
+$version = [System.Reflection.Assembly]::LoadFile("$root\Mongo.Migration\bin\Release\net45\Mongo.Migration.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
