3686a3d27c036ffd2501a5af06455dacada4d49a
sroddis <sean_roddis@hotmail.de>
2019-05-11 13:00:41 +0200

    updated versions of test unr

--- post_build.cmd
+++ post_build.cmd
@@ -1,7 +1,7 @@
-nuget install NUnit.Runners -Version 3.2.1 -OutputDirectory tools
-nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
-nuget install coveralls.net -Version 0.6.0 -OutputDirectory tools
+nuget install NUnit.Runners -Version 3.10.0 -OutputDirectory tools
+nuget install OpenCover -Version 4.67.882 -OutputDirectory tools
+nuget install coveralls.net -Version 1.0.0 -OutputDirectory tools
  
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe -targetargs:".\Mongo.Migration.Test\bin\Release\Mongo.Migration.Test.dll" -filter:"+[Mongo.Migration]* -[Mongo.Migration.Test]*" -register:user
+.\tools\OpenCover.4.67.882\tools\OpenCover.Console.exe -target:.\tools\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe -targetargs:".\Mongo.Migration.Test\bin\Release\Mongo.Migration.Test.dll" -filter:"+[Mongo.Migration]* -[Mongo.Migration.Test]*" -register:user
  
-.\tools\coveralls.net.0.6.0\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
+.\tools\coveralls.net.1.0.0\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
