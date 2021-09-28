727c8246f4a7bf75c5b8a377d893673288c72d37
Sean Roddis <sean_roddis@hotmail.de>
2021-09-28 14:49:14 +0200

    remove appveyor

--- post_build.cmd
+++
@@ -1,7 +0,0 @@
-nuget install NUnit.Runners -Version 3.12.0 -OutputDirectory tools
-nuget install OpenCover -Version 4.7.1221 -OutputDirectory tools
-nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
- 
-.\tools\OpenCover.4.7.1221\tools\OpenCover.Console.exe -target:.\tools\NUnit.ConsoleRunner.3.12.0\tools\nunit3-console.exe -targetargs:".\Mongo.Migration.Test\bin\Release\Mongo.Migration.Test.dll" -filter:"+[Mongo.Migration]* -[Mongo.Migration.Test]*" -register:user
- 
-.\tools\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
